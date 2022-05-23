a944d0d6f3fafd9593dd5330ad9c6dcdedae05b9
od3ng <noprianto@polinema.ac.id>
2022-05-23 11:35:31 +0700

    expo polinema

--- src/main.cpp
+++ src/main.cpp
@@ -104,7 +104,7 @@ const char *wifiPass = "90071126";
   ------------------------------------------------------------------------------*/
 const char *brokerUser = NULL;
 const char *brokerPass = NULL;
-const char *brokerHost = "192.168.0.107";
+const char *brokerHost = "192.168.0.101";
 // const char *brokerHost = "192.168.43.85";
 
 /*----------------------------------------------------
